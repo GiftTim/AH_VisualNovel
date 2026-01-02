d60326a3556ca629a3021043067fa48530351b72
June <95timv@gmail.com>
2026-01-03 01:04:05 +0900

    EP15-1

---
+++ Assets/Scripts/VisualNovel/Core/Audio/AudioManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance { get; private set; }
+
+    public AudioMixerGroup musicMixer;
+    public AudioMixerGroup sfxMixer;
+    public AudioMixerGroup voiceMixer;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            transform.SetParent(null);
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+    }
+}
