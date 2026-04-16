b86b03e201fad300341c73de3093d2055fa909a2
June <95timv@gmail.com>
2026-04-17 08:45:08 +0900

    Day2
    미치겠다 왜 안되지?

--- Assets/Scripts/StrategyGame/Core/PathFinder/Spot.cs
+++ Assets/Scripts/StrategyGame/Core/PathFinder/Spot.cs
@@ -1,52 +1,32 @@
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public enum SpotType
+public enum BuildingType
 {
-    HQ,          // 본부
-    Idle,        // 사건 없는 건물
+    HQ,          // 본부건물
+    Normal,      // 기본 건물
+
+}
+
+public enum SpotState
+{
+    Idle,        // 대기 중인 건물
     Incident,    // 사건 발생 건물
     Unselected,  // 선택 안 된 건물 (미션 만료)
     Selected,    // 선택된 건물 (미션 진행 중)
     Completed    // 사건 완료 건물
 }
 
-[System.Serializable]
-public class SpotVisualConfig
-{
-    public SpotType type;
-    public Color color = Color.white;
-    public Sprite sprite; // null이면 스프라이트 유지
-}
-
 public class Spot : MonoBehaviour
 {
-    [SerializeField] SpotType _spotType;
-    [SerializeField] List<Spot> _neighbors;
-    [SerializeField] Image _icon;
-    [SerializeField] SpotVisualConfig[] _visualConfigs;
+    [SerializeField] BuildingType _spotType;
+    public BuildingType SpotType => _spotType;
 
-    public SpotType SpotType => _spotType;
-    public List<Spot> Neighbors => _neighbors;
 
-    void Start() => ApplyVisual();
-
-    public void SetType(SpotType type)
+    public void SetType(BuildingType type)
     {
         _spotType = type;
-        ApplyVisual();
+
     }
 
-    void ApplyVisual()
-    {
-        foreach (var config in _visualConfigs)
-        {
-            if (config.type != _spotType) continue;
-            _icon.color = config.color;
-            if (config.sprite != null)
-                _icon.sprite = config.sprite;
-            return;
-        }
-    }
+
 }
